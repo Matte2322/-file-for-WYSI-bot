e594258870c49ce7754eba1991c50f395adb2421
Matte2322 <71736367+Matte2322@users.noreply.github.com>
2022-01-30 13:09:26 -0800

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -23,17 +23,19 @@ try:
 except:
     print("Error during authentication")
 
-
+time1 = '7:27 AM/PM'
 def task():
-    api.update_status(f"HOLY SHIT, WHEN YOU SEE IT. Printed out at 7:27 AM/PM UTC.")
+    api.update_status(f"HOLY SHIT, WHEN YOU SEE IT. Printed out at {time1} UTC.")
+        
 
 # converted local time to universal time 
 schedule.every().day.at("23:27").do(task)
 schedule.every().day.at("11:27").do(task)
 
+
 while True:
     schedule.run_pending()
-    time.sleep(1)
+    time.sleep(1)   
 
 
 
